fc7c9877857df1c17852cc3dabb45413f27c5af1
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-01-26 21:54:39 +0500

    Артем Авдеев/используем-If by Survivor of the SAO

---
+++ 005_FavoritName/Program.cs
@@ -0,0 +1,15 @@
+﻿//by Survivor of the SAO
+//----------------------
+
+Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+
+if (username=="жопа")
+{
+    Console.Write("Сам ты -");
+    Console.WriteLine(username);
+}
+    else
+    {
+        Console.Write("Привет, "); Console.WriteLine(username);
+    }
