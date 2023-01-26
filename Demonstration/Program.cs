b530ad97a2aaa8b8936ae19109756ce434103051
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-01-26 22:45:05 +0500

    Артем Авдеев/семинар1 практика 1 by Survivor of the SAO

---
+++ Demonstration/Program.cs
@@ -0,0 +1,8 @@
+﻿// By Survivor of the SAO
+//------------------------
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result=number*number;
+System.Console.WriteLine("Квадрат чила = "+ result);
