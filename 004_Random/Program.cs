97e9e660c005aeb6f5ed3ac92470d57f218a3f6d
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-01-26 21:36:56 +0500

    Артем Авдеев  прогаN4  на C# - сложение рандомов by Survivor of the SAO

---
+++ 004_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿// by Survivor of the SAO
+//-----------------------
+int numA= new Random().Next(1,10);  //от 1 до 9
+int numB= new Random().Next(1,10);  //от 1 до 9
+int result=numA+numB;
+
+Console.Write(result);Console.Write("=");Console.Write(numA);Console.Write("+");Console.WriteLine(numB);
