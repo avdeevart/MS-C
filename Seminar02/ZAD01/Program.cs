d9f19994282782c59cd50bf733c58df37bbd8834
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-01-31 16:33:41 +0500

    Артем Авдеев/семинар02 ZAD01 вывод3х и уд2-го   by Survivor of the SAO

--- Seminar02/ZAD01/Program.cs
+++ Seminar02/ZAD01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Рандомим 3х-значное число и удаляет вторую цифру этого числа
+
+Random rand = new Random();
+int number = rand.Next(100,1000);
+System.Console.WriteLine(number);
+
+// int leftnum = number/10;
+// int rightnum= number%10;
+
+System.Console.WriteLine($"{number/100} {(number-((number/100)*100))-(((number-((number/100)*100))/10)*10)}");
