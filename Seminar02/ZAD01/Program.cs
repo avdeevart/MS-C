ca7991365901507df5cd02cbf8994cc40195ea0d
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-01-31 16:36:58 +0500

    Артем Авдеев/семинар02 ZAD01 вывод3х и уд2-го   by Survivor of the SAO

--- Seminar02/ZAD01/Program.cs
+++ Seminar02/ZAD01/Program.cs
@@ -4,7 +4,4 @@ Random rand = new Random();
 int number = rand.Next(100,1000);
 System.Console.WriteLine(number);
 
-// int leftnum = number/10;
-// int rightnum= number%10;
-
 System.Console.WriteLine($"{number/100} {(number-((number/100)*100))-(((number-((number/100)*100))/10)*10)}");
