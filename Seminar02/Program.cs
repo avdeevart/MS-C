31cfabdfc5df15d1c0e0cea3dea79a080ce2d6cc
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-01-31 14:31:51 +0500

    Артем Авдеев/семинар02 задачка: что больше  by Survivor of the SAO

--- Seminar02/Program.cs
+++ Seminar02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывод случайного числа из отрезка [10,99] и покзывает наибольшую цифру числа
+
+// int number = new Random().Next(10,100); //класс рандомных чисел в next - 
+                                        // указывает диапазон. причем крайнее число не включается, поэтому +1
+
+Random rand = new Random();
+int number = rand.Next(10,100);
+System.Console.WriteLine(number);
+
+// int leftnum = number/10;
+// int rightnum= number%10;
+
+System.Console.WriteLine($"{number/10} {number%10}");
+
+if (number/10>number%10) {System.Console.WriteLine($"{number/10}>{number%10}");}
+    
+    else if (number/10<number%10) {System.Console.WriteLine($"{number/10}<{number%10}");}
+            
+            else {System.Console.WriteLine($"{number/10}<{number%10}");}
