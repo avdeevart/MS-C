f043b99d2fed550b18a2fbbabda98b7ec97c9001
SAO Survivor <120559747+avdeevart@users.noreply.github.com>
2023-02-05 18:33:41 +0500

    Артем Авдеев/семинар03 через функции и switch/case/brake  by Survivor of the SAO

---
+++ Seminar03/ZAD18/Program.cs
@@ -0,0 +1,33 @@
+﻿// По заданному номеру четверти, показать диапазон возможных координат 
+//_____________________________________________________________________________________________________________________________
+void RangeDefinition (int numQuarter)   //объявление метода по выводу диапазона
+{
+    switch(numQuarter)
+    {
+        case <1:
+        case >4:
+            System.Console.WriteLine($"Четверти нумеруютя от 1 до 4, а Вы ввели: {numQuarter}");
+            break;
+        case 1:
+            System.Console.WriteLine($"Четверть - {numQuarter}, диапазон X>0; Y>0");
+            break;
+        case 2:
+            System.Console.WriteLine($"Четверть - {numQuarter}, диапазон X<0; Y>0");
+            break;
+        case 3:
+            System.Console.WriteLine($"Четверть - {numQuarter}, диапазон X>0; Y<0");
+            break;
+        case 4:
+            System.Console.WriteLine($"Четверть - {numQuarter}, диапазон X<0; Y<0");
+            break;
+    }
+}
+//_____________________________________________________________________________________________________________________________
+int InputCord(string str)   // объявление метода ввода четверти
+{
+    System.Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+//_____________________________________________________________________________________________________________________________
+
+RangeDefinition(InputCord("Введите номер четверти (от 1 до 4):"));
